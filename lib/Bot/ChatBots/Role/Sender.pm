3994e7955062b790b9f355c6c3e4151b45418f06
Flavio Poletti <flavio@polettix.it>
2016-11-08 18:40:35 +0100

    Add Sender role

---
+++ lib/Bot/ChatBots/Role/Sender.pm
@@ -0,0 +1,40 @@
+package Bot::ChatBots::Role::Sender;
+use strict;
+{ our $VERSION = '0.001006'; }
+
+use Moo::Role;
+requires 'send_message';
+
+has callback => (
+   is        => 'rw',
+   lazy      => 1,
+   predicate => 1,
+   clearer   => 1,
+);
+
+has ua => (
+   is      => 'rw',
+   lazy    => 1,
+   default => 'BUILD_ua',
+);
+
+sub BUILD_ua {
+   my $self = shift;
+   require Mojo::UserAgent;
+   return Mojo::UserAgent->new;
+}
+
+sub ua_request {
+   my ($self, $method, @args) = @_;
+   $method = lc $method;
+   my @callback =
+       (scalar(@args) && (ref($args[-1] eq 'CODE'))) ? (pop @args)
+     : $self->has_callback ? ($self->callback)
+     :                       ();
+   my $res = $self->ua->$method(@args, @callback);
+   Mojo::IOLoop->start
+     if (!Mojo::IOLoop->is_running) && scalar(@callback);
+   return $res;
+} ## end sub ua_request
+
+1;
