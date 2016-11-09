abf534b10546f1c2c3364826a2e93467af2c3f04
Flavio Poletti <flavio@polettix.it>
2016-11-09 17:25:40 +0100

    Adjust ua_request()

--- lib/Bot/ChatBots/Role/Sender.pm
+++ lib/Bot/ChatBots/Role/Sender.pm
@@ -19,6 +19,11 @@ has recipient => (
    clearer   => 1,
 );
 
+has start_loop => (
+   is      => 'rw',
+   default => sub { return 0 },
+);
+
 has ua => (
    is      => 'rw',
    lazy    => 1,
@@ -31,16 +36,31 @@ sub BUILD_ua {
    return Mojo::UserAgent->new;
 }
 
+sub may_start_loop {
+   my ($self, %args) = @_;
+   my $start_loop =
+     exists($args{start_loop})
+     ? $args{start_loop}
+     : $self->start_loop;
+   Mojo::IOLoop->start if $start_loop && (!Mojo::IOLoop->is_running);
+   return $self;
+} ## end sub may_start_loop
+
 sub ua_request {
-   my ($self, $method, @args) = @_;
+   my ($self, $method, %args) = @_;
    $method = lc $method;
+
+   my @ua_args = @{$args{ua_args}};
+
    my @callback =
-       (scalar(@args) && (ref($args[-1] eq 'CODE'))) ? (pop @args)
+       (scalar(@ua_args) && (ref($ua_args[-1] eq 'CODE'))) ? (pop @ua_args)
      : $self->has_callback ? ($self->callback)
      :                       ();
-   my $res = $self->ua->$method(@args, @callback);
-   Mojo::IOLoop->start
-     if (!Mojo::IOLoop->is_running) && scalar(@callback);
+
+   my $res = $self->ua->$method(@ua_args, @callback);
+
+   $self->may_start_loop(%args) if @callback;
+
    return $res;
 } ## end sub ua_request
 
