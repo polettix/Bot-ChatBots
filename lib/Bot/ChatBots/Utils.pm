477684aced58fb195cc4bd1e892ee7c0c82b7f1a
Flavio Poletti <flavio@polettix.it>
2016-11-11 13:57:24 +0100

    Tests and stuff for ::Util

--- lib/Bot/ChatBots/Utils.pm
+++ lib/Bot/ChatBots/Utils.pm
@@ -2,8 +2,10 @@ package Bot::ChatBots::Utils;
 use strict;
 { our $VERSION = '0.001015'; }
 
+use 5.010;
 use Exporter 'import';
 use Module::Runtime qw< use_module >;
+use Ouch;
 
 our @EXPORT_OK = qw< load_module pipeline resolve_module >;
 
@@ -12,25 +14,17 @@ sub load_module { return use_module(resolve_module(@_)) }
 sub pipeline {
    return $_[0] if (@_ == 1) && (ref($_[0]) eq 'CODE');
 
-   my $opts = {};
-   $opts = shift(@_) if (@_ && ref($_[0]) eq 'HASH');
-   $opts = pop(@_)   if (@_ && ref($_[-1]) eq 'HASH');
-   my $prefix = $opts->{prefix} // 'Bot::ChatBots';
-   my @defs = map {
-      my $r = ref $_;
-      if (!$r) {
-         '!' . resolve_module($_, $prefix);
-      }
-      elsif ($r eq 'ARRAY') {
-         my ($name, @rest) = @$_;
-         ['!' . resolve_module($_, $prefix), @rest];
-      }
-      else { $_ }
-   } @_;
-   require Data::Tubes;
-   $opts->{tap} //= sub { ($_[0]->())[0] }
-     unless exists $opts->{pump};
-   Data::Tubes::pipeline(@defs, $opts);
+   state $loaded = eval "use Data::Tubes '0.736'; 1"
+     or ouch 500, 'need Data::Tubes at least 0.736 for pipeline()';
+
+   my %opts;
+   %opts = %{shift(@_)} if (@_ && ref($_[0]) eq 'HASH');
+   %opts = %{pop(@_)}   if (@_ && ref($_[-1]) eq 'HASH');
+   $opts{prefix} //= 'Bot::ChatBots';
+   $opts{tap} //= sub { ($_[0]->())[0] }
+     unless exists $opts{pump};
+
+   return Data::Tubes::pipeline(@defs, \%opts);
 } ## end sub pipeline
 
 sub resolve_module {
